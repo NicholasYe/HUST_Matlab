8b5409def8293e0f204270ecdfefccbd16370028
NicholasYe <2216393769@qq.com>
2022-03-29 18:31:37 +0800

    3.29_18:31

--- 3.第二次课/dsolve_equations.m
+++ 3.第二次课/dsolve_equations.m
@@ -1,12 +1,12 @@
-% set up function
+%% set up function
 clc, clear all, close all;
 syms y(x)
 eqn = (x*diff(y,x)+y-exp(x));
 
-% calculate
+%% calculate
 S = dsolve(eqn, y(1)==2*exp(1));
 
-% draw picture
+%% draw picture
 m = -10:0.1e-2:10;
 n = (exp(m)-exp(1)+(3060513257434037/562949953421312))./m;
 plot(m, n, '-', LineWidth=2);
